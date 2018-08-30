b343c9aacb7ba0dc125a80387069029c06222732
sushen <sushenbiswasaga@gmail.com>
2018-08-30 06:45:38 +0600

    Initial commit

---
+++ ThisBlockchainSiteDo.py
@@ -0,0 +1,18 @@
+#coinmarketcap.com, hitbtc.com, localbitcoins.com, sushenbiswas.com
+
+website = ''
+
+if website == 'coinmarketcap.com':
+    print("Coinmarketcap Give you a preview of Crypto currency Market.")
+
+elif website == 'hitbtc.com':
+    print("hitbtc is a crypto currency exchange site.")
+
+elif website == 'localbitcoins.com':
+    print("localbitcoins is a crypto currency buy/sell site.")
+
+elif website == 'sushenbiswas.com':
+    print("sushenbiswas is a crypto currency Tutorial site.")
+
+else:
+    print(" You type Some thing Wrong!")

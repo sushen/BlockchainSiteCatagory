0039358b0a2461b65b479dfa0bd0ce399ac3041f
sushen <sushenbiswasaga@gmail.com>
2018-09-01 08:14:15 +0600

    This script make decision What you need to do whit your Cripto-currency.

---
+++ DetectMarketChange.py
@@ -0,0 +1,18 @@
+bitcoinPrise =9000
+dominance = 44
+
+if bitcoinPrise >= 10000 and dominance >=50:
+    print("Sell The Bitcoin and Liquafy local Currency.")
+
+elif bitcoinPrise <= 10000 and dominance >=50:
+    print("Sell The Bitcoin Buy USDT")
+
+elif bitcoinPrise >= 10000 and dominance <=50:
+    print("Sell The Bitcoin Buy Ripple, Etherium and Bitcoin Cash")
+
+elif bitcoinPrise <= 10000 and dominance <= 50:
+        print("Find New Block-chain Technology and buy there ICO")
+
+else:
+    print(" Dont Know use your brain")
+

e376cbd7998aabd700466ed2f4f5861872732a92
sushen <sushenbiswasaga@gmail.com>
2018-09-02 15:55:26 +0600

    I try to decorate it some way.

---
+++ TopTenCriptocurrency.py
@@ -0,0 +1,21 @@
+topCripto = ['Bitcoin', 'Ethereum', 'XRP', 'Bitcoin Cash', 'EOS', 'Stellar','Litecoin', 'Tether']
+
+uppercase_Cripto = []
+lowercase_cripto = []
+topCriptoIndex = []
+
+for Cripto in topCripto:
+    uppercase_Cripto.append(Cripto.upper())
+    lowercase_cripto.append(Cripto.lower())
+
+for Cripto_Index in range(len(topCripto)):
+    topCriptoIndex.append(Cripto_Index)
+
+for money in range(len(topCripto)):
+    topCripto[money] = topCripto[money]
+
+print(lowercase_cripto)
+print(uppercase_Cripto)
+print(topCriptoIndex)
+
+print(topCripto)

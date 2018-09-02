98b64b548934c55c988d74176d2a1e15721e5bbf
sushen <sushenbiswasaga@gmail.com>
2018-09-02 18:28:01 +0600

    cryptocurrency Ranking using Dictionary and loop.

---
+++ CriptoRankoforBuy.py
@@ -0,0 +1,16 @@
+topcripto = {
+           "Bitcoin BTC": "Number_One First_hundert",
+           "Theta_Token THETA": "Number_One Secound_hundert",
+           "DigitalNote XDN": "Number_One Third_hundert",
+           "VITE": "Number_One Forth_hundert"
+       }
+
+for cripto in topcripto:
+    print(cripto)
+
+print("\n")
+
+for cripto, value in topcripto.items():
+    print("Cripto Currency:  {}      Ranking:  {}".format(cripto,value))
+
+

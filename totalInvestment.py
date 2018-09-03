3b4ad2801f4c2e9231431719c40fb5c5b4f52fad
sushen <sushenbiswasaga@gmail.com>
2018-09-03 09:14:43 +0600

    Total Investment in coin Market

--- totalInvestment.py
+++ totalInvestment.py
@@ -1,6 +1,10 @@
 
 total_investment = 0
+
+#Grab hole data from coinmarketcap
 Coins = {'Bitcoin' :7254.18, 'Ethereum':292.88 ,'XRP':0.340238, 'Bitcoin Cash':635.71 }
+
+#Make a gui selector for which coin I invest
 invested_coins = ['Bitcoin','Bitcoin Cash']
 
 for invested_coin, price in Coins.items():

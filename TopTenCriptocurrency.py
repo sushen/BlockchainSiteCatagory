3f1fb9d1b0f174c00558c32f48c6cbb77a7952b7
sushen <sushenbiswasaga@gmail.com>
2018-09-03 10:57:21 +0600

    Import Criptocurrency Data using Coinmarketcap API.

--- TopTenCriptocurrency.py
+++ TopTenCriptocurrency.py
@@ -1,21 +1,22 @@
 topCripto = ['Bitcoin', 'Ethereum', 'XRP', 'Bitcoin Cash', 'EOS', 'Stellar','Litecoin', 'Tether']
 
-uppercase_Cripto = []
-lowercase_cripto = []
-topCriptoIndex = []
+#uppercase_Cripto = []
+#lowercase_cripto = []
+#topCriptoIndex = []
 
-for Cripto in topCripto:
-    uppercase_Cripto.append(Cripto.upper())
-    lowercase_cripto.append(Cripto.lower())
+#for Cripto in topCripto:
+    #uppercase_Cripto.append(Cripto.upper())
+    #lowercase_cripto.append(Cripto.lower())
 
-for Cripto_Index in range(len(topCripto)):
-    topCriptoIndex.append(Cripto_Index)
+#for Cripto_Index in range(len(topCripto)):
+    #topCriptoIndex.append(Cripto_Index)
 
 for money in range(len(topCripto)):
-    topCripto[money] = topCripto[money]
+    topCripto[money] = topCripto[money].upper()
 
-print(lowercase_cripto)
-print(uppercase_Cripto)
-print(topCriptoIndex)
+#print(lowercase_cripto)
+#print(uppercase_Cripto)
+
+#print(topCriptoIndex)
 
 print(topCripto)
